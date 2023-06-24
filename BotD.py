e4eb0063af9ea6e3962a72e6c417e9ca1dadcede
Saratean <135757567+Saratean@users.noreply.github.com>
2023-06-24 16:30:49 +0200

    Add files via upload

---
+++ BotD.py
@@ -0,0 +1,39 @@
+from settings import settings
+import discord
+from discord.ext import commands
+from bot_logic import *
+
+
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+
+
+bot.run(settings["TOKEN"])
+
+
+
+
+
+
+
+
+
